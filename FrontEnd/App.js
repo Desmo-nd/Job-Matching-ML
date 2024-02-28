f6442cd8620fc628a8383cfcf82236c7411b6e56
Desmo-nd <mwrgdesmond@gmail.com>
2024-02-28 23:59:29 +0300

    home

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Home from './screens/Home'; // Update the path to match your file structure
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    regular:require("./assets/fonts/Poppins-Regular.ttf"),
+    light:require("./assets/fonts/Poppins-Light.ttf"),
+    bold:require("./assets/fonts/Poppins-Bold.ttf"),
+    medium:require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold:require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold:require("./assets/fonts/Poppins-SemiBold.ttf"),
+
+
+  })
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
